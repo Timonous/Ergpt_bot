575d0661a87b0b26c9ab39e3f09ac2daf2cc30b7
Артём Либенсон <artem.russia.perm@gmail.com>
2025-06-02 00:27:25 +0500

    feat: Создана функция creat_pool для подключения к бд

---
+++ db.py
@@ -0,0 +1,12 @@
+import asyncpg
+from settings import config
+
+# Функция создаёт пул подключений к PostgreSQL
+async def create_pool():
+    return await asyncpg.create_pool(
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        database=config.DB_NAME,
+        host=config.DB_HOST,
+        port=config.DB_PORT
+    )
