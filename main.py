1a8c7242f6f00500678b846091e4a8fe06f44517
Артём Либенсон <artem.russia.perm@gmail.com>
2025-06-13 00:18:33 +0500

    feat: Добавил задачу удаления старых чатов в автозапуск

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeAllGroupChats
 from db import create_pool
 from settings import config
 from bot.handlers import router as main_router
 from bot.group_handlers import router as group_router
 from bot.auth import router as auth_router, set_db_pool, daily_staff_status_check
-from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeAllGroupChats
+from bot.api.ergpt import daily_chats_delete
 
 
 
@@ -42,6 +43,7 @@ async def main() -> None:
     # Подключение к БД
     db_pool = await create_pool()
     set_db_pool(db_pool)
+    await daily_chats_delete()
 
     bot = Bot(token=config.BOT_TOKEN)
     await set_bot_commands(bot)
@@ -55,6 +57,7 @@ async def main() -> None:
     #Настройка автозапуска функции
     scheduler = AsyncIOScheduler()
     scheduler.add_job(daily_staff_status_check, CronTrigger(hour=00, minute=00))
+    scheduler.add_job(daily_chats_delete, CronTrigger(hour=2, minute=00))
     scheduler.start()
 
     await dp.start_polling(bot)
